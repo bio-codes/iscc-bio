263d8a3496a5279cb8d91567b2f9455bc4650954
Titusz Pan <titusz.pan@gmail.com>
2025-10-04 15:46:20 +0200

    feat: silence bioformats Java logging output
    Add _silence_bioformats_logging() function to disable verbose Java logging from bioformats loci package. Also standardize logger import from 'log' to 'logger' for consistency.

--- iscc_bio/__init__.py
+++ iscc_bio/__init__.py
@@ -1,16 +1,34 @@
 import os
 from .utils import find_java_home
-from loguru import logger as log
+from loguru import logger
 
 if not os.environ.get("JAVA_HOME"):
     java_home = find_java_home()
     if java_home:
         os.environ["JAVA_HOME"] = java_home
-        log.debug(f"Auto-detected JAVA_HOME: {java_home}")
+        logger.debug(f"Auto-detected JAVA_HOME: {java_home}")
     else:
-        log.warning(
+        logger.warning(
             "Warning: Could not auto-detect Java installation. bioio-bioformats may not work."
         )
-        log.warning("Please install Java and/or set JAVA_HOME environment variable.")
+        logger.warning("Please install Java and/or set JAVA_HOME environment variable.")
+
+
+def _silence_bioformats_logging():
+    """Silence Java logging from bioformats loci package."""
+    try:
+        import scyjava
+        import jpype
+
+        scyjava.config.endpoints.append("ome:formats-gpl:6.7.0")
+        scyjava.start_jvm()
+        loci = jpype.JPackage("loci")
+        loci.common.DebugTools.setRootLevel("OFF")
+    except Exception:
+        # Silently fail if bioformats is not available or already initialized
+        pass
+
+
+_silence_bioformats_logging()
 
 __version__ = "0.1.0"
